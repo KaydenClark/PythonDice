6f96d01fa6f88da78d3e4332b17b41d199fbb79e
Kayden Clark <kaydenclark725@gmail.com>
2020-04-03 00:28:38 -0600

    added D10

--- dice.py
+++ dice.py
@@ -9,6 +9,8 @@ def d6():
     print("D6", randint(1,6))
 def d8():
     print("D8", randint(1,8))
+def d10():
+    print("D10", randint(1, 10))
 def d12():
     print("D12", randint(1,12))
 def d20():
@@ -23,7 +25,7 @@ app = Frame(root)
 app.pack
 label =Label(app,text = "Dice Things")
 label.pack()
-root.title("Dice thingingy")
+root.title("Pocket Dice")
 
 d4 = Button(root, text= "D4", bg= "black", fg= "white", command= d4)
 d4.pack(fill=X)
@@ -31,8 +33,11 @@ d4.pack(fill=X)
 d6 = Button(root, text= "D6", bg= "blue", fg= "white", command= d6)
 d6.pack(fill=X)
 
-d8 = Button(root, text= "D8", bg= "purple", fg= "white", command= d8)
+d8 = Button(root, text= "D8", bg= "yellow", fg= "black", command= d8)
 d8.pack(fill=X)
+
+d10 = Button(root, text= "D10", bg= "purple", fg= "white", command= d8)
+d10.pack(fill=X)
 
 d12 = Button(root, text= "D12", bg= "green", fg= "white", command= d12)
 d12.pack(fill=X)
@@ -40,13 +45,13 @@ d12.pack(fill=X)
 d20 = Button(root, text= "D20", bg= "red", fg= "white", command= d20)
 d20.pack(fill=X)
 
-d100 = Button(root, text= "D100", bg= "orange", fg= "white", command= d100)
+d100 = Button(root, text= "D100", bg= "orange", fg= "black", command= d100)
 d100.pack(fill=X)
 
 percentageDie = Button(root, text= "Percentage Die", command= percentageDie)
 percentageDie.pack(fill=X)
 
-root.geometry("500x204")
+root.geometry("500x220")
 root.mainloop()
 
 
